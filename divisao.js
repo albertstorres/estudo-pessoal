c6ac96c961d00dd9a6b7edce3ce7f54b235f4582
albertstorres <albertstorres@gmail.com>
2023-07-06 22:02:57 -0300

    PROGRAMA QUE EXECUTA DIVISAO DE DOIS VALORES

---
+++ divisao.js
@@ -0,0 +1,11 @@
+let divisor = 8;
+let dividendo = 40;
+let divisao = 0;
+
+if (divisor != 0) {
+    divisao = dividendo / divisor;
+    console.log(dividendo, " / ", divisor, " = ", divisao);
+} else {
+    console.log("OPERAÇÃO INVÁLIDA!");
+}
+
